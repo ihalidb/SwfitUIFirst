b19cf1e585d03df6ebc61d0ba9fbc16f37daa7e0
İbrhaim Halid Bayrak <ibrahimh.bayrak@gmail.com>
2023-08-13 12:52:44 +0300

    Initial Commit

---
+++ DetailView/DetailViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  DetailViewApp.swift
+//  DetailView
+//
+//  Created by İbrahim Halid Bayrak on 13.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct DetailViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
